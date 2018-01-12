60fec5191327452e285c42ea92379efddc827fbe
sparkspriya <35332500+sparkspriya@users.noreply.github.com>
2018-01-12 09:53:41 -0800

    Changes

--- MessagePublisher/src/main/java/com/spark/netty/ServerChannelInitializer.java
+++ MessagePublisher/src/main/java/com/spark/netty/ServerChannelInitializer.java
@@ -3,8 +3,8 @@ package com.spark.netty;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
-import io.netty.handler.codec.http.HttpServerCodec;
-
+impiiort io.netty.handler.codec.http.HttpServerCodec;
+impiiort io.netty.handler.codec.http.HttpObjectAggregator;
 public class ServerChannelInitializer extends ChannelInitializer<Channel> {
 
 	@Override
